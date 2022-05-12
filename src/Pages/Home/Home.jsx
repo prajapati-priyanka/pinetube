492a5187422cd912f4be4ab7c976f5a9ebb8993c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-12 13:04:17 +0530

    feat: Import Navbar, Sidebar, VideoContent

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,30 +1,15 @@
-import { HiOutlineMenu,HiOutlineUser } from "react-icons/hi";
-import { BsSearch  } from "react-icons/bs";
+import { useState } from "react";
+import { Navbar,SideNav,VideoContent } from "../../Components";
 import "./Home.css";
 
 const Home = () => {
+  const [sideNavShrinked, setSideNavShrinked] = useState(false)
   return (
-    <nav className="nav-wrapper flex-container">
-      <section className="nav-left flex-container">
-        <HiOutlineMenu className="menu-bar lg-text"/>
-        <img src="../assets/logo.png" className="logo-img" alt="" />
-       
-      </section>
-      <section className="nav-middle">
-      <input
-          type="search"
-          placeholder="Search Product"
-          className="input-search"
-        />
-        <button type="submit" className="btn-search">
-          <BsSearch />
-        </button>
-      </section>
-      <section className="nav-right flex-container">
-          <HiOutlineUser className="user-icon lg-text"/>
-         <p className="user-login md-text">Login</p>
-      </section>
-    </nav>
+         <>
+         <Navbar sideNavShrinked={sideNavShrinked} setSideNavShrinked={setSideNavShrinked}/>
+         <SideNav sideNavShrinked={sideNavShrinked} setSideNavShrinked={setSideNavShrinked}/>
+         <VideoContent sideNavShrinked={sideNavShrinked} />
+         </>
   );
 };
 
