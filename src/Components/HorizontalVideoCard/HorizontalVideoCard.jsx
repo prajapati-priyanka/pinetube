aeea4a18db2fe4776b768802d25f70692fd44f4c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-13 16:29:45 +0530

    feat: Add HorizontalVideoCard

---
+++ src/Components/HorizontalVideoCard/HorizontalVideoCard.jsx
@@ -0,0 +1,28 @@
+import "./HorizontalVideoCard.css";
+import {BiDotsVerticalRounded} from  "react-icons/bi";
+
+const HorizontalVideoCard = ({img, videoCreator, likes, views, desc}) =>{
+    return(
+        <div className="video-card horizontal-video-card">
+        <a href="">
+          <img
+            src={img}
+            alt=""
+            className="thumbnail"
+          />
+        </a>
+
+        <div className="video-info">
+          <a href="">
+           {desc}
+          </a>
+          <p>{videoCreator}</p>
+          <p>{views} | {likes}</p>
+        </div>
+        <button className="btn dot-btn lg-text">
+                <BiDotsVerticalRounded />
+       </button>
+      </div>
+    )
+}
+export{HorizontalVideoCard}
