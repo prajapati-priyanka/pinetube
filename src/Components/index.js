4aed611b41e7c3b701e4495e193958456ab7dc72
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-14 14:52:26 +0530

    feat: Import VideoPlayer, VideoContent

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,4 +2,5 @@ export {Navbar} from "./Navbar/Navbar";
 export {SideNav} from "./SideNav/SideNav";
 export {VideoCard} from "./VideoCard/VideoCard";
 export{HorizontalVideoCard} from "./HorizontalVideoCard/HorizontalVideoCard";
-export {VideoPlayer} from "./VideoPlayer/VideoPlayer"
+export {VideoPlayer} from "./VideoPlayer/VideoPlayer";
+export {VideoContent} from "./VideoContent/VideoContent"
