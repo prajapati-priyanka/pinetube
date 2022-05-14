1680a1d6f59abda86248415f277e7c4af8f633d9
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-14 23:00:09 +0530

    feat: Add PlaylistPage

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,4 +1,5 @@
 export {Home} from "./Home/Home";
 export {NotFound} from "./404Page/404Page";
 export {SingleVideoPage} from "./SingleVideoPage/SingleVideoPage";
-export {SinglePlaylistPage} from "./SinglePlaylistPage/SinglePlaylistPage"
+export {SinglePlaylistPage} from "./SinglePlaylistPage/SinglePlaylistPage";
+export {PlaylistPage} from "./PlaylistPage/PlaylistPage"
