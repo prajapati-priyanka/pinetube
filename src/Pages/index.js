5ce6887e3004472bc9331d91e1a7e23bdca662c7
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-15 16:55:17 +0530

    feat: Add VideoListingPage

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,4 +2,5 @@ export {Home} from "./Home/Home";
 export {NotFound} from "./404Page/404Page";
 export {SingleVideoPage} from "./SingleVideoPage/SingleVideoPage";
 export {SinglePlaylistPage} from "./SinglePlaylistPage/SinglePlaylistPage";
-export {PlaylistPage} from "./PlaylistPage/PlaylistPage"
+export {PlaylistPage} from "./PlaylistPage/PlaylistPage";
+export {VideoListingPage} from "./VideoListingPage/VideoListingPage"
