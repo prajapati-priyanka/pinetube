443941ed9591d422351212345429acaf99bd49c3
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-16 00:56:55 +0530

    fix: Remove states passed as props

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,12 +4,12 @@ import { VideoListingPage } from "../VideoListingPage/VideoListingPage";
 import "./Home.css";
 
 const Home = () => {
-  const [sideNavShrinked, setSideNavShrinked] = useState(false)
+ 
   return (
          <>
-         <Navbar sideNavShrinked={sideNavShrinked} setSideNavShrinked={setSideNavShrinked}/>
-         <SideNav sideNavShrinked={sideNavShrinked} setSideNavShrinked={setSideNavShrinked}/>
-         <VideoListingPage sideNavShrinked={sideNavShrinked} />
+         <Navbar />
+         <SideNav />
+         <VideoListingPage />
          </>
   );
 };
