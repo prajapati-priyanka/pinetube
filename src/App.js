0e65d1814add7da0c451ece646a739a84b641843
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-16 00:41:16 +0530

    feat: Add Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
-import {Home, SingleVideoPage, PlaylistPage, SinglePlaylistPage, NotFound } from "./Pages";
+import {Home, SingleVideoPage, PlaylistPage, SinglePlaylistPage, NotFound, Liked, WatchLater, History } from "./Pages";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-       <Home />
-       {/* <SingleVideoPage /> */}
-       {/* <PlaylistPage /> */}
-       {/* <SinglePlaylistPage /> */}
-       {/* <NotFound /> */}
+    <Routes>
+       <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/signup" element={<Signup />} /> */}
+        <Route path="/playlists" element={<PlaylistPage />} />
+        <Route path="/singlevideopage" element={<SingleVideoPage />} />
+        <Route path="/singleplaylistpage" element={<SinglePlaylistPage />} />
+        <Route path="/liked" element={<Liked />} />
+        <Route path="/watchlater" element={<WatchLater />} />
+        <Route path="/history" element={<History />} />
+        <Route path="*" element={<NotFound />} />
+      
+      </Routes>
+    
     </div>
   );
 }
