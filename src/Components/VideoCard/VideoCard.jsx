5dcf5efb4c251224116bfb39c8756916aca29cec
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-15 21:55:59 +0530

    feat: Add video length

--- src/Components/VideoCard/VideoCard.jsx
+++ src/Components/VideoCard/VideoCard.jsx
@@ -3,9 +3,10 @@ import {BiDotsVerticalRounded} from  "react-icons/bi";
 import { VideoMenu } from "./VideoMenu";
 import { useState } from "react";
 
-const VideoCard=({img,channelImg, desc, views, likes, videoCreator})=>{
+const VideoCard=({img,channelImg, desc, views, likes, videoCreator,isPlaylistModalVisible, setIsPlaylistModalVisible})=>{
    
-    const [isVideoMenuVisible,setIsVideoMenuVisible] = useState(false)
+    const [isVideoMenuVisible,setIsVideoMenuVisible] = useState(false);
+  
 
     return(
         <div className="video-card">
@@ -20,8 +21,18 @@ const VideoCard=({img,channelImg, desc, views, likes, videoCreator})=>{
             <button className="btn dot-btn lg-text" onClick={()=>setIsVideoMenuVisible(isVideoMenuVisible => !isVideoMenuVisible)}>
                 <BiDotsVerticalRounded />
             </button>
+            
+            <div class="icon">
+              <span class="badge-count">21:4</span>
+            </div>
            
-           {isVideoMenuVisible ? <VideoMenu setIsVideoMenuVisible={setIsVideoMenuVisible} isVideoMenuVisible={isVideoMenuVisible} /> : null}
+           {isVideoMenuVisible ? <VideoMenu 
+           setIsVideoMenuVisible={setIsVideoMenuVisible} 
+           isVideoMenuVisible={isVideoMenuVisible} 
+           isPlaylistModalVisible={isPlaylistModalVisible}
+           setIsPlaylistModalVisible={setIsPlaylistModalVisible}
+           
+           /> : null}
 
         </div>
     </div>
