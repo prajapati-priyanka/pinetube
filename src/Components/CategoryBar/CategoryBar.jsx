5c27466ad1fafe5dbfced3de47d383ed7c6343be
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-15 22:43:53 +0530

    feat: Add CategoryBar

---
+++ src/Components/CategoryBar/CategoryBar.jsx
@@ -0,0 +1,21 @@
+import "./CategoryBar.css"
+
+const CategoryBar = () =>{
+    return(
+   <div className="category-bar flex-container">
+       <button className="btn filter-category md-text">Comedy</button>
+       <button className="btn filter-category md-text">Sprituality</button>
+       <button className="btn filter-category md-text">90s songs</button>
+       <button className="btn filter-category md-text">90s songs</button>
+       <button className="btn filter-category md-text">90s songs</button>
+       <button className="btn filter-category md-text">90s songs</button>
+       <button className="btn filter-category md-text">90s songs</button>
+       <button className="btn filter-category md-text">Programming</button>
+       <button className="btn filter-category md-text">Programming</button>
+       <button className="btn filter-category md-text">Programming</button>
+       <button className="btn filter-category md-text">Programming</button>
+   </div>
+    )
+}
+
+export {CategoryBar}
