5c45c151da0558398855a7ca1a64b9c57d57961a
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-16 23:12:10 +0530

    fix: Import useEffect hook

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,5 +1,6 @@
 import { createContext,useContext,useReducer } from "react";
 import { authReducer } from "../reducer/auth-reducer";
+import { useEffect } from "react";
 
 const authInitialState = {
     user: "",
