fc0438e25eb8c60d080068c19f2e5936159a5dae
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-16 23:12:39 +0530

    feat: Add RequiresAuth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import "./App.css";
-import {Home, SingleVideoPage, PlaylistPage, SinglePlaylistPage, NotFound, Liked, WatchLater, History, Login, Signup } from "./Pages";
+import {
+  Home,
+  SingleVideoPage,
+  PlaylistPage,
+  SinglePlaylistPage,
+  NotFound,
+  Liked,
+  WatchLater,
+  History,
+  Login,
+  Signup,
+} from "./Pages";
 import { Routes, Route } from "react-router-dom";
-
+import { RequiresAuth } from "./Router/RequiresAuth";
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-       <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/playlists" element={<PlaylistPage />} />
+
         <Route path="/singlevideopage" element={<SingleVideoPage />} />
         <Route path="/singleplaylistpage" element={<SinglePlaylistPage />} />
-        <Route path="/liked" element={<Liked />} />
-        <Route path="/watchlater" element={<WatchLater />} />
-        <Route path="/history" element={<History />} />
+        <Route
+          path="/liked"
+          element={<RequiresAuth children={<Liked />}></RequiresAuth>}
+        />
+        <Route
+          path="/watchlater"
+          element={<RequiresAuth children={<WatchLater />}></RequiresAuth>}
+        />
+        <Route
+          path="/history"
+          element={<RequiresAuth children={<History />}></RequiresAuth>}
+        />
+        <Route
+          path="/playlists"
+          element={<RequiresAuth children={<PlaylistPage />}></RequiresAuth>}
+        />
         <Route path="*" element={<NotFound />} />
-      
       </Routes>
-    
     </div>
   );
 }
