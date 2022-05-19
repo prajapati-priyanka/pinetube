07b70c5bbd7b77dd44d1bcaaa90bc78d0c8f7b00
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-19 13:46:38 +0530

    feat: Change data with video data

--- src/Components/VideoCard/VideoCard.jsx
+++ src/Components/VideoCard/VideoCard.jsx
@@ -4,19 +4,20 @@ import { VideoMenu } from "./VideoMenu";
 import { useState } from "react";
 import {Link} from "react-router-dom"
 
-const VideoCard=({img,channelImg, desc, views, likes, videoCreator,isPlaylistModalVisible, setIsPlaylistModalVisible})=>{
+const VideoCard=({videos,isPlaylistModalVisible,setIsPlaylistModalVisible})=>{
    
     const [isVideoMenuVisible,setIsVideoMenuVisible] = useState(false);
-  
+
+    const {title,channelName,channelThumbnail, videoThumbnail,views,likes,videoLength} = videos
 
     return(
         <div className="video-card">
-        <Link to="/singlevideopage"><img src={img} alt="" className="thumbnail" /></Link>
+        <Link to="/singlevideopage"><img src={videoThumbnail} alt="" className="thumbnail" /></Link>
         <div className="flex-container">
-            <img src={channelImg} alt="" />
+            <img src={channelThumbnail} alt="" />
             <div className="video-info">
-                <Link to="/singlevideopage">{desc}</Link>
-                <p>{videoCreator}</p>
+                <Link to="/singlevideopage">{title}</Link>
+                <p>{channelName}</p>
                 <p>{views} | {likes}</p>
             </div>
             <button className="btn dot-btn lg-text" onClick={()=>setIsVideoMenuVisible(isVideoMenuVisible => !isVideoMenuVisible)}>
@@ -24,7 +25,7 @@ const VideoCard=({img,channelImg, desc, views, likes, videoCreator,isPlaylistMod
             </button>
             
             <div className="icon">
-              <span className="badge-count">21:4</span>
+              <span className="badge-count">{videoLength}</span>
             </div>
            
            {isVideoMenuVisible ? <VideoMenu 
