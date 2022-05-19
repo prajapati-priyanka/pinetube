b8638a07c63ea220b843f8c9a79e3b9b6ef3f6dc
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-19 12:32:10 +0530

    feat: Add videos to database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,159 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "Nj4H-X3FAWU",
+    title: "Hera Pheri All Best Comedy Scenes",
+    channelName: "Goldmines Bollywood",
+    views: "83.84M",
+    likes:"570K",
+    videoLength:"30:53",
+    channelThumbnail: "https://yt3.ggpht.com/cpajxwo_F6AznpciPGhZs7TLBJiwRM02BR67VkI_p0I99vR2VvQ2S6ZRm6zLoBf0y9voDqU92bs=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/Nj4H-X3FAWU/sddefault.jpg",
+    category: "comedies"
+
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "Y2Oj9gllHno",
+    title: "I am a Chef | Aakash Gupta | Stand-Up Comedy | Crowd Work",
+    channelName: "Akash Gupta",
+    views: "9.7M",
+    likes:"743K",
+    videoLength:"8:54",
+    channelThumbnail: "https://yt3.ggpht.com/ytc/AKedOLSvPEUzOL_qQkQQd17lcsvj3ikeb2iEj92MUBi91A=s176-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/Y2Oj9gllHno/sddefault.jpg",
+    category: "comedies",
+    subscribers:"3.17M"
+
+  },
+  {
+    _id: "qkxuFKqJXWY",
+    title: "Roommate - Stand Up Comedy Ft. Anubhav Singh Bassi",
+    channelName: "Anubhav Singh Bassi",
+    views: "5.3M",
+    likes:"2.3M",
+    videoLength:"22:44",
+    channelThumbnail: "https://yt3.ggpht.com/ytc/AKedOLS7IEPwzVpf3MOKIBHsLlIIG_UTH7CKO2iKYNxw4A=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/qkxuFKqJXWY/sddefault.jpg",
+    category: "comedies",
+    subscribers:"3.17M"
+
+  },
+  {
+    _id: "qkxuFKqJXWY",
+    title: "Manoj Bajpayee & Anubhav Sinhas's Funny Banter",
+    channelName: "Kapil Sharma",
+    views: "1.2M",
+    likes:"21K",
+    videoLength:"22:44",
+    channelThumbnail: "https://www.youtube.com/watch?v=HHuKF-njaAk",
+    videoThumbnail: "https://img.youtube.com/vi/HHuKF-njaAk/sddefault.jpg",
+    category: "comedies",
+    subscribers:"8.01M"
+
+  },
+  {
+    _id: "kl3qt66O2zk",
+    title: "The Kapil Sharma Season 2",
+    channelName: "LIV Comedy",
+    views: "5.1M",
+    likes:"6.8K",
+    videoLength:"8:48",
+    channelThumbnail: "https://yt3.ggpht.com/C8dX6T92VPU3AaY3ECmlTzZ4hFbHmPEt9JlL-zZOc4QWcKVdoCVrKrI_m8obNXU2b4I0sO8n=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/kl3qt66O2zk/sddefault.jpg",
+    category: "comedies",
+    subscribers:"5.51M"
+
+  },
+  {
+    _id: "vtg9q00AZOc",
+    title: "How To Overcome Loneliness?",
+    channelName: "Sadhguru",
+    views: "998K",
+    likes:"5.9K",
+    videoLength:"9:46",
+    channelThumbnail: "https://yt3.ggpht.com/ytc/AKedOLRuuRLqxnh4b91JgJz_5BpBPC9oAf7eGzy3N_pcqDU=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/vtg9q00AZOc/sddefault.jpg",
+    category: "Motivational",
+    subscribers:"9.84M"
+
+  },
+  {
+    _id: "cwLVRzAGjkw",
+    title: "Suffering From Lack of Self-Confidence?",
+    channelName: "Sadhguru",
+    views: "319K",
+    likes:"12K",
+    videoLength:"9:46",
+    channelThumbnail: "https://yt3.ggpht.com/ytc/AKedOLRuuRLqxnh4b91JgJz_5BpBPC9oAf7eGzy3N_pcqDU=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/cwLVRzAGjkw/sddefault.jpg",
+    category: "Motivational",
+    subscribers:"9.84M"
+
+  },
+  {
+    _id: "zUZ-gglWOw4",
+    title: "Is comfort zone ruining your life?",
+    channelName: "warikoo",
+    views: "425k",
+    likes:"25k",
+    videoLength:"5:20",
+    channelThumbnail: "https://yt3.ggpht.com/X9eoDIB9cgb1s-kvATRs1lQDcU4Fjc15NDV9s9FF8ck7IsA8u7OdijaernoDV9LLdePgjlt_=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/zUZ-gglWOw4/sddefault.jpg",
+    category: "Motivational",
+    subscribers:"1.81M"
+
+  },
+ 
+  {
+    _id: "KwXtalUuhLc",
+    title: "Sudha Murthy - So What If I Lost The Battle, I Lived The War",
+    channelName: "BeerBiceps",
+    views: "1.42M",
+    likes:"46k",
+    videoLength:"47:16",
+    channelThumbnail: "https://yt3.ggpht.com/4syhBAa6d4sK_J8YxhRFHDyARee3qoR2bUtGMtRDyyVncFn41bSHUDf-FNmvcu3TD2JwowD8sA=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/KwXtalUuhLc/sddefault.jpg",
+    category: "Motivational",
+    subscribers:"3.96M"
+
+  },
+  {
+    _id: "0NV1KdWRHck",
+    title: "Thoughts on humanity, fame and love | Shah Rukh Khan",
+    channelName: "TED",
+    views: "8.52M",
+    likes:"251K",
+    videoLength:"17:51",
+    channelThumbnail: "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/0NV1KdWRHck/sddefault.jpg",
+    category: "TED TALKS",
+    subscribers:"21.2M"
+
+  },
+  {
+    _id: "42cwiP25ChI",
+    title: "Why patience is a superpower | Oliver Burkeman",
+    channelName: "TEDx Talks",
+    views: "56K",
+    likes:"2K",
+    videoLength:"17:02",
+    channelThumbnail: "https://yt3.ggpht.com/ytc/AKedOLQGnY6McntoY-eWaRDI047FWFqj7pVlw2nBfkwz7w=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/42cwiP25ChI/sddefault.jpg",
+    category: "TED TALKS",
+    subscribers:"34.8M"
+
+  },
+  {
+    _id: "Hp0vG274DAM",
+    title: "Life Begins at the End of your Comfort Zone | Mayank Mishra",
+    channelName: "TEDx Talks",
+    views: "6.7K",
+    likes:"446",
+    videoLength:"17:57",
+    channelThumbnail: "https://yt3.ggpht.com/ytc/AKedOLQGnY6McntoY-eWaRDI047FWFqj7pVlw2nBfkwz7w=s88-c-k-c0x00ffffff-no-rj",
+    videoThumbnail: "https://img.youtube.com/vi/42cwiP25ChI/sddefault.jpg",
+    category: "TED TALKS",
+    subscribers:"34.8M"
+
   },
 ];
