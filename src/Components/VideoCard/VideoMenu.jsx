f365f656de8ba2cbe907e4a4f83a9cf51b494dae
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-19 18:48:05 +0530

    feat: Add LikeClickHandler function

--- src/Components/VideoCard/VideoMenu.jsx
+++ src/Components/VideoCard/VideoMenu.jsx
@@ -1,12 +1,28 @@
 import { MdPlaylistAdd,MdOutlineWatchLater} from "react-icons/md";
 import { AiOutlineLike} from "react-icons/ai";
+import { useAuth, useLike } from "../../context";
+import { useNavigate } from "react-router-dom";
+import { toggleLikeHandler } from "../../helper/like-helper";
 
-const VideoMenu = ({setIsPlaylistModalVisible, setIsVideoMenuVisible})=>{
+
+const VideoMenu = ({setIsPlaylistModalVisible, setIsVideoMenuVisible, videos})=>{
+  const {authState} = useAuth();
+  const {likeDispatch, likeState:{likeData}} = useLike();
+  const navigate = useNavigate();
+  const token = authState.token || localStorage.getItem("token");
 
   const openPlaylistHandler = ()=>{
     setIsPlaylistModalVisible(true);
     setIsVideoMenuVisible(false)
   }
+
+ const isVideoInLikePage =  likeData.find(item => item._id === videos._id);
+  
+ 
+ const likeClickHandler=()=>{
+     setIsVideoMenuVisible(false);
+     toggleLikeHandler(token,likeDispatch, isVideoInLikePage, navigate, videos)
+    }
  
     return(
       <>
@@ -20,11 +36,12 @@ const VideoMenu = ({setIsPlaylistModalVisible, setIsVideoMenuVisible})=>{
           <MdOutlineWatchLater className="menu-row-icon lg-text"/>
           <span className="icon-name md-text">Save to Watch Later</span>
          </button>
-
-        <button className="btn menu-row flex-container" title="Liked">
+    
+       <button className="btn menu-row flex-container" onClick={likeClickHandler}>
           <AiOutlineLike className="menu-row-icon lg-text" />
-          <span className="icon-name md-text">Add To Liked Videos</span>
+          <span className="icon-name md-text">{(isVideoInLikePage && token) ? "Remove from" : "Add To"} liked Videos</span>
         </button>
+     
         </div>
 
 
