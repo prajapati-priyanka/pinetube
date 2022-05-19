86a65464c657ef9a589baf88d01510d892c02120
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-19 18:46:19 +0530

    fix: Add key prop

--- src/Components/CategoryBar/CategoryBar.jsx
+++ src/Components/CategoryBar/CategoryBar.jsx
@@ -4,7 +4,7 @@ const CategoryBar = ({allCategories}) =>{
     return(
    <div className="category-bar flex-container">
        <button className="btn filter-category md-text">All</button>
-       {allCategories.map(category => <button className="btn filter-category md-text">{category.categoryName}</button> )}
+       {allCategories.map(category => <button key={category._id} className="btn filter-category md-text">{category.categoryName}</button> )}
     
     </div>
     )
