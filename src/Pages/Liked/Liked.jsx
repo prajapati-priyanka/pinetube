fb3297a3c5b889ecbc33d04c26bd1823a1c35d27
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-19 18:49:31 +0530

    feat: Update liked page

--- src/Pages/Liked/Liked.jsx
+++ src/Pages/Liked/Liked.jsx
@@ -1,60 +1,71 @@
-import { HorizontalVideoCard, Navbar, SideNav } from "../../Components"
+import { HorizontalVideoCard, Navbar, SideNav } from "../../Components";
+import { useLike } from "../../context";
 
-const Liked = () =>{
-    return(
-        <>
-        <Navbar />
-        <SideNav />
-        <div className="main-container playlist-page">
-          <div className="row">
-         <div className="playlist-left-content">
-         <div className="card ecommerce-card" >  
-        <div className="card-with-text-overlay">
-          <figure className="card-header">
-            <img src ="../assets/channel/channel.jpg" alt="" />
-          </figure>
-        </div>
-        <div className="text-overlay lg-text">Videos</div>
-      </div>
-       
-       <div className="playlist-info">
-         <h3 className="playlist-name">Liked Videos</h3>
-         <p className="playlist-updated-time">Updated on May 10,2022</p>
-       </div>
+const Liked = () => {
+  const {
+    likeState: { likeData },
+  } = useLike();
+  return (
+    <>
+      <Navbar />
+      <SideNav />
+      <div className="main-container playlist-page">
+        <div className="row">
+          <div className="playlist-left-content">
+            <div className="card ecommerce-card">
+              <div className="card-with-text-overlay">
+                {likeData.length > 0 ? (
+                  <figure className="card-header">
+                    <img src={likeData[0].videoThumbnail} alt="" />
+                  </figure>
+                ) : (
+                  <figure className="card-header">
+                    <img src="./assets/defaultImg.jpg" alt="default-img" />
+                  </figure>
+                )}
+              </div>
+              <div className="text-overlay lg-text">
+                Videos({likeData.length})
+              </div>
+            </div>
 
-      <div className="divider"></div>
+            <div className="playlist-info">
+              <h3 className="playlist-name">Liked Videos</h3>
+              <p className="playlist-updated-time">Updated on May 10,2022</p>
+            </div>
 
-      <div className="video-creator">
-          <img src="./assets/channel/channel.jpg" alt="" />
-          <div className="video-channel">
-            <p className="channel-name md-text">Priyanka Prajapati</p>
-            <span className="channel-subscribers">50K subscribers</span>
+            <div className="divider"></div>
+
+            <div className="video-creator">
+              <img
+                src="https://lh3.googleusercontent.com/ogw/ADea4I4uUERI95xonyxzL3nGOOMdNrpjNtGEXURXLFL0=s32-c-mo"
+                alt=""
+              />
+              <div className="video-channel">
+                <p className="channel-name md-text">Priyanka Prajapati</p>
+                <span className="channel-subscribers">50K subscribers</span>
+              </div>
+            </div>
           </div>
-        </div> 
 
-         </div>
-         
-
-      <div className="playlist-right-content">
-      <HorizontalVideoCard
-              img="../assets/thumbnails/thumbnail.jpg"
-              desc="The Kapil Sharma Show | Baisakhi Special | Comedy Circus"
-              videoCreator="The Kapil Sharma Show"
-              likes="23M likes"
-              views="15M views"
-            />
-      </div>
-         
+          <div className="playlist-right-content">
+            {likeData.length < 1 ? (
+              <div className="lg-text text-center">
+                No Data in the liked page
+              </div>
+            ) : (
+              likeData.map((likeVideo) => (
+                <HorizontalVideoCard
+                  key={likeVideo._id}
+                  likeVideo={likeVideo}
+                />
+              ))
+            )}
           </div>
-          
-
-           
-
-           
-           
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export {Liked}
+export { Liked };
