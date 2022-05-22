ff43617af7c302aff6c313bd131ba8348dccf534
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-22 15:16:24 +0530

    feat: Add navigate to singleplaylist page

--- src/Pages/PlaylistPage/PlaylistPage.jsx
+++ src/Pages/PlaylistPage/PlaylistPage.jsx
@@ -1,57 +1,64 @@
 import "./PlaylistPage.css";
-import { MdPlaylistAdd} from "react-icons/md";
-import { BsTrash} from "react-icons/bs";
-import {Navbar, SideNav} from "../../Components"
+import { MdPlaylistAdd } from "react-icons/md";
+import { BsTrash } from "react-icons/bs";
+import { Navbar, SideNav } from "../../Components";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth, usePlaylist } from "../../context";
 import { deletePlaylistService } from "../../services";
 
+const PlaylistPage = () => {
+  const navigate = useNavigate();
+  const {
+    playlistState: { playlists },
+    playlistDispatch,
+  } = usePlaylist();
+  const {
+    authState: { token },
+  } = useAuth();
 
-const PlaylistPage = () =>{
-   
-    const navigate= useNavigate();
-    const {playlistState : {playlists}, playlistDispatch} = usePlaylist();
-    const{authState: {token}} = useAuth();
+  const deletePlaylistHandler = (playlistId) => {
+    deletePlaylistService(token, playlistId, playlistDispatch);
+  };
 
-    const playlistPageHandler = () =>{
-        navigate("/singleplaylistpage")
-    }
+  return (
+    <>
+      <Navbar />
+      <SideNav />
 
-    const deletePlaylistHandler = (playlistId) =>{
-        deletePlaylistService(token, playlistId, playlistDispatch)
-    }
+      <div className="main-container">
+        <h3 className="page-title">Playlist</h3>
 
-    return(
-        <>
-        <Navbar />
-        <SideNav />
-       
-       <div className="main-container">
-           <h3 className="page-title">Playlist</h3>
-         
-              
-                {playlists.length > 0 ? (playlists.map(playlist => (
-                     <div key={playlist._id}  className="playlist flex-container"> 
-                     <MdPlaylistAdd className="playlist-icon x-lg-text"/>
-                   <div onClick={playlistPageHandler}>
-                       <p className="playlist-name lg-text">{playlist.title}</p>
-                       <p className="playlist-name text-muted">{playlist.videos.length} {playlist.videos.length ===1 ? "Video" : "Videos"}</p>
-                   </div>
-                   <button className="btn trash-icon lg-text" onClick={()=> deletePlaylistHandler(playlist)}>
-                        <BsTrash />
-                   </button>
-                   </div>
-                ))) : (
-                    <Link to="/explore">
-                    <button className="btn btn-primary">Explore</button>
-                    </Link>
-                )}
-                  
+        {playlists.length > 0 ? (
+          playlists.map((playlist) => (
+            <div
+              key={playlist._id}
+              className="playlist flex-container"
+              onClick={() => navigate(`/playlists/${playlist._id}`)}
+            >
+              <MdPlaylistAdd className="playlist-icon x-lg-text" />
+              <div>
+                <p className="playlist-name lg-text">{playlist.title}</p>
+                <p className="playlist-name text-muted">
+                  {playlist.videos.length}{" "}
+                  {playlist.videos.length === 1 ? "Video" : "Videos"}
+                </p>
+              </div>
+              <button
+                className="btn trash-icon lg-text"
+                onClick={() => deletePlaylistHandler(playlist)}
+              >
+                <BsTrash />
+              </button>
+            </div>
+          ))
+        ) : (
+          <Link to="/explore">
+            <button className="btn btn-primary">Explore</button>
+          </Link>
+        )}
+      </div>
+    </>
+  );
+};
 
-       
-    </div>
-       </>
-    )
-}
-
-export {PlaylistPage}
+export { PlaylistPage };
