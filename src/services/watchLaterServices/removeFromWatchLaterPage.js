7a9c3e27ee4673fde868274bb03a400d976fdb59
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-23 02:00:29 +0530

    feat: Add network call to remove video from watch later

---
+++ src/services/watchLaterServices/removeFromWatchLaterPage.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const removeFromWatchLaterPage = async (videos,token,watchLaterDispatch) =>{
+try {
+    const response = await axios.delete(`/api/user/watchlater/${videos._id}`, {headers:{
+        authorization: token
+    }});
+   
+ if(response.status === 200){
+        watchLaterDispatch({type: "DELETE_FROM_WATCH_LATER", payload: response.data.watchlater});
+        toast.error("Removed from Watch Later Page")
+    }else{
+        throw new Error("Can't process the request")
+    }
+} catch (error) {
+    console.log(error)
+}
+}
