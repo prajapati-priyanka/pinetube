0076a67cd9e911dfa66ce1e2c39c981b74fcb408
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-22 13:31:42 +0530

    feat: Create addVideoToPlaylist service

---
+++ src/services/playlistServices/addVideoToPlaylist.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+export const addVideoToPlaylist = async (
+  token,
+  playlistId,
+  videos,
+  playlistDispatch
+) => {
+  try {
+    const response = await axios.post(
+      `/api/user/playlists/${playlistId}`,
+      { video: videos },
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+
+    if (response.status === 201) {
+      playlistDispatch({
+        type: "ADD_TO_PLAYLIST",
+        payload: response.data.playlist,
+      });
+      toast.success("Video Added To Playlist");
+    } else {
+      throw new Error("Can't process the request");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
