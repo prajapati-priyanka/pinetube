8c1d99180b9dc9263a93fb8e995593041707f1ac
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-23 00:10:21 +0530

    fix: Change toast

--- src/services/playlistServices/removeVideoFromPlaylist.js
+++ src/services/playlistServices/removeVideoFromPlaylist.js
@@ -1,17 +1,26 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const removeVideoFromPlaylist = async (token, playlistId,video ,playlistDispatch)=>{
-    try {
-        const response = await axios.delete(`/api/user/playlists/${playlistId}/${video._id}`, {headers: {authorization : token}})
+export const removeVideoFromPlaylist = async (
+  token,
+  playlistId,
+  video,
+  playlistDispatch
+) => {
+  try {
+    const response = await axios.delete(
+      `/api/user/playlists/${playlistId}/${video._id}`,
+      { headers: { authorization: token } }
+    );
 
-        console.log(response)
-        if(response.status === 200){
-            playlistDispatch({type: "REMOVE_FROM_PLAYLIST", payload: response.data.playlist});
-            toast.success("Video removed from playlist")
-        }
-        
-    } catch (error) {
-        console.log(error)
+    if (response.status === 200) {
+      playlistDispatch({
+        type: "REMOVE_FROM_PLAYLIST",
+        payload: response.data.playlist,
+      });
+      toast.error("Video removed from playlist");
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
