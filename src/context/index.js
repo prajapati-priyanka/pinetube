90914f6f02a06a201cdf3d69aabd8c459df17f74
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-22 13:44:18 +0530

    feat: Add PlaylistProvider

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
 export {SideNavProvider, useSideNav} from "./sidenav-context";
 export {AuthProvider, useAuth} from "./auth-context";
 export {LikeProvider, useLike} from "./like-context";
+export {PlaylistProvider, usePlaylist} from "./playlist-context";
