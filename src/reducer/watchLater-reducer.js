8d3dacf716690dea919890cf06885c26c1975142
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-23 01:56:11 +0530

    feat: Create watchLater reducer

---
+++ src/reducer/watchLater-reducer.js
@@ -0,0 +1,17 @@
+export const watchLaterReducer = (state,action) =>{
+    switch (action.type) {
+        case "ADD_TO_WATCH_LATER":
+            return{
+                ...state,
+                watchLater: action.payload
+            }
+        case "DELETE_FROM_WATCH_LATER":
+            return{
+                ...state,
+                watchLater: action.payload
+            }
+    
+        default:
+            return state
+    }
+}
