1e445733a74cb5a5b8eca176abacd18f9c09e72a
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-22 13:45:01 +0530

    feat: Add function calls

--- src/Components/Modal/PlaylistModal.jsx
+++ src/Components/Modal/PlaylistModal.jsx
@@ -2,9 +2,66 @@ import "./PlaylistModal.css";
 import { BsXLg } from "react-icons/bs";
 import { useState } from "react";
 import { MdPlaylistAdd } from "react-icons/md";
+import { useAuth, usePlaylist } from "../../context";
+import { createNewPlaylistService,addVideoToPlaylist, removeVideoFromPlaylist } from "../../services"
 
-const PlaylistModal = ({ setIsPlaylistModalVisible }) => {
-  const [isCreatePlaylistVisible, setIsCreatePlaylistVisible] = useState(false);
+const PlaylistModal = ({ setIsPlaylistModalVisible, playlistVideo }) => {
+  const [isCreatePlaylistInputVisible, setIsCreatePlaylistInputVisible] =
+    useState(false);
+  const [newPlaylistData, setNewPlaylistData] = useState({
+    title: "",
+    description: "Hello World",
+  });
+
+  const {
+    playlistState: { playlists },
+    playlistDispatch,
+  } = usePlaylist();
+  const { authState } = useAuth();
+
+  const token = authState.token || localStorage.getItem("token");
+
+  const createPlaylistHandler = () => {
+    if (!isCreatePlaylistInputVisible) {
+      setIsCreatePlaylistInputVisible(true);
+    }
+    if (
+      newPlaylistData.title !== "" &&
+      playlists.findIndex(
+        (item) => (item.title !== newPlaylistData.title) === -1
+      )
+    ) {
+      createNewPlaylistService(token, newPlaylistData, playlistDispatch);
+    }
+
+    setNewPlaylistData({ title: "", description: "" });
+  };
+
+  const handlePlaylistInputChange = (e) => {
+    setNewPlaylistData({ ...newPlaylistData, title: e.target.value });
+  };
+
+  const checkIsVideoInPlaylist = (playlistId) => {
+    const isPlaylistExist = playlists.find((item) => item._id === playlistId);
+
+    if (isPlaylistExist) {
+      return isPlaylistExist.videos.some(
+        (item) => item._id === playlistVideo._id
+      );
+    } else {
+      return false;
+    }
+  };
+
+  const CheckPlaylistInputHandler = (e) => {
+    const { checked, id } = e.target;
+    
+    if (checked) {
+      addVideoToPlaylist(token, id, playlistVideo, playlistDispatch);
+    } else {
+      removeVideoFromPlaylist(token, id, playlistVideo, playlistDispatch);
+    }
+  };
 
   return (
     <div className="modal-background">
@@ -27,22 +84,40 @@ const PlaylistModal = ({ setIsPlaylistModalVisible }) => {
               Watch Later
             </label>
           </div>
+
+          {playlists.length > 0
+            ? playlists.map((item) => (
+                <div key={item._id} className="input-container">
+                  <input
+                    type="checkbox"
+                    id={item._id}
+                    checked={checkIsVideoInPlaylist(item._id)}
+                    onChange={CheckPlaylistInputHandler}
+                  />
+                  <label htmlFor={item._id} className="md-text">
+                    {item.title}
+                  </label>
+                </div>
+              ))
+            : null}
         </section>
 
         <div className="divider"></div>
 
         <footer className="create-playlist-footer">
-          {isCreatePlaylistVisible ? (
+          {isCreatePlaylistInputVisible ? (
             <input
               type="text"
               className="create-playlist-input"
               placeholder="My Playlist"
+              value={newPlaylistData.title}
+              onChange={handlePlaylistInputChange}
             />
           ) : null}
 
           <button
             className="btn flex-container md-text create-playlist-btn"
-            onClick={() => setIsCreatePlaylistVisible(true)}
+            onClick={createPlaylistHandler}
           >
             <MdPlaylistAdd />
             <span>Create Playlist</span>
