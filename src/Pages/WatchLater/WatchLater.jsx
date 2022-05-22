dc3e3c2fe4a4cdf769bf015398e08e52abbfea73
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-23 01:56:55 +0530

    feat: Add ContentSidebar component

--- src/Pages/WatchLater/WatchLater.jsx
+++ src/Pages/WatchLater/WatchLater.jsx
@@ -1,60 +1,41 @@
-import { HorizontalVideoCard, Navbar, SideNav } from "../../Components"
+import {
+  ContentSidebar,
+  HorizontalVideoCard,
+  Navbar,
+  SideNav,
+} from "../../Components";
+import { useWatchLater } from "../../context";
 
-const WatchLater = () =>{
-    return(
-        <>
-        <Navbar />
-        <SideNav />
-        <div className="main-container playlist-page">
-          <div className="row">
-         <div className="playlist-left-content">
-         <div className="card ecommerce-card" >  
-        <div className="card-with-text-overlay">
-          <figure className="card-header">
-            <img src ="../assets/channel/channel.jpg" alt="" />
-          </figure>
-        </div>
-        <div className="text-overlay lg-text">Videos</div>
-      </div>
-       
-       <div className="playlist-info">
-         <h3 className="playlist-name">Watch Later</h3>
-         <p className="playlist-updated-time">Updated on May 10,2022</p>
-       </div>
+const WatchLater = () => {
+  const {
+    watchLaterState: { watchLater },
+  } = useWatchLater();
+  return (
+    <>
+      <Navbar />
+      <SideNav />
+      <div className="main-container playlist-page">
+        <div className="row">
+          <ContentSidebar videoData={watchLater} />
 
-      <div className="divider"></div>
-
-      <div className="video-creator">
-          <img src="./assets/channel/channel.jpg" alt="" />
-          <div className="video-channel">
-            <p className="channel-name md-text">Priyanka Prajapati</p>
-            <span className="channel-subscribers">50K subscribers</span>
+          <div className="playlist-right-content">
+            {watchLater.length < 1 ? (
+              <div className="lg-text text-center">
+                No Data in the watchLater page
+              </div>
+            ) : (
+              watchLater.map((watchLaterVideo) => (
+                <HorizontalVideoCard
+                  key={watchLaterVideo._id}
+                  video={watchLaterVideo}
+                />
+              ))
+            )}
           </div>
-        </div> 
-
-         </div>
-         
-
-      <div className="playlist-right-content">
-      <HorizontalVideoCard
-              img="../assets/thumbnails/thumbnail.jpg"
-              desc="The Kapil Sharma Show | Baisakhi Special | Comedy Circus"
-              videoCreator="The Kapil Sharma Show"
-              likes="23M likes"
-              views="15M views"
-            />
-      </div>
-         
-          </div>
-          
-
-           
-
-           
-           
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export {WatchLater}
+export { WatchLater };
