827d0ac14c35efa9272997ac8f171356879f88b0
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-23 00:10:55 +0530

    refactor: remove console.log

--- src/services/playlistServices/createNewPlaylistService.js
+++ src/services/playlistServices/createNewPlaylistService.js
@@ -5,7 +5,7 @@ export const createNewPlaylistService = async (token, newPlaylistData, playlistD
     
     try { 
         const response = await axios.post("/api/user/playlists", { playlist: newPlaylistData }, {headers:{authorization: token}});
-         console.log(response)
+       
         if(response.status === 201){
             playlistDispatch({type: "CREATE_PLAYLIST", payload: response.data.playlists});
             toast.success("new playlist created")
