25865d0dbc0b7b8a07f93a66cfcc3d084875e84e
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-24 02:14:32 +0530

    feat: Add Navigation to singleVideoPage

--- src/Components/VideoCard/VideoCard.jsx
+++ src/Components/VideoCard/VideoCard.jsx
@@ -1,46 +1,64 @@
-import "./VideoCard.css"
-import {BiDotsVerticalRounded} from  "react-icons/bi";
+import "./VideoCard.css";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 import { VideoMenu } from "./VideoMenu";
 import { useState } from "react";
-import {Link} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-const VideoCard=({videos,setIsPlaylistModalVisible, setPlaylistVideo})=>{
-   
-    const [isVideoMenuVisible,setIsVideoMenuVisible] = useState(false);
+const VideoCard = ({ videos, setIsPlaylistModalVisible, setPlaylistVideo }) => {
+  const [isVideoMenuVisible, setIsVideoMenuVisible] = useState(false);
+  const navigate = useNavigate();
 
-    const {title,channelName,channelThumbnail, videoThumbnail,views,likes,videoLength} = videos
+  const {
+    title,
+    channelName,
+    channelThumbnail,
+    videoThumbnail,
+    views,
+    likes,
+    videoLength,
+  } = videos;
 
-    return(
-        <div className="video-card">
-        <Link to="/singlevideopage"><img src={videoThumbnail} alt="" className="thumbnail" /></Link>
-        <div className="flex-container">
-            <img src={channelThumbnail} alt="" />
-            <div className="video-info">
-                <Link to="/singlevideopage">{title}</Link>
-                <p>{channelName}</p>
-                <p>{views} | {likes}</p>
-            </div>
-            <button className="btn dot-btn lg-text" onClick={()=>setIsVideoMenuVisible(isVideoMenuVisible => !isVideoMenuVisible)}>
-                <BiDotsVerticalRounded />
-            </button>
-            
-            <div className="icon">
-              <span className="badge-count">{videoLength}</span>
-            </div>
-           
-           {isVideoMenuVisible ? <VideoMenu 
-           setIsVideoMenuVisible={setIsVideoMenuVisible} 
-           isVideoMenuVisible={isVideoMenuVisible} 
-           setIsPlaylistModalVisible={setIsPlaylistModalVisible}
-           videos={videos}
-           setPlaylistVideo={setPlaylistVideo}
-   
-           
-           /> : null}
+  const videoNavigation = () => {
+    navigate(`/explore/${videos._id}`);
+  };
 
+  return (
+    <div className="video-card" onClick={videoNavigation}>
+      <img src={videoThumbnail} alt="" className="thumbnail" />
+      <div className="flex-container">
+        <img src={channelThumbnail} alt="" />
+        <div className="video-info">
+          <p className="title">{title}</p>
+          <p className="channel-name">{channelName}</p>
+          <p className="views-likes">
+            {views} | {likes}
+          </p>
         </div>
-    </div>
-    )
-}
+        <button
+          className="btn dot-btn lg-text"
+          onClick={(e) =>{ e.stopPropagation()
+            setIsVideoMenuVisible((isVideoMenuVisible) => !isVideoMenuVisible)
+          }}
+        >
+          <BiDotsVerticalRounded />
+        </button>
 
-export {VideoCard}
+        <div className="icon">
+          <span className="badge-count">{videoLength}</span>
+        </div>
+
+        {isVideoMenuVisible ? (
+          <VideoMenu
+            setIsVideoMenuVisible={setIsVideoMenuVisible}
+            isVideoMenuVisible={isVideoMenuVisible}
+            setIsPlaylistModalVisible={setIsPlaylistModalVisible}
+            videos={videos}
+            setPlaylistVideo={setPlaylistVideo}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export { VideoCard };
