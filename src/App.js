4f259079eb02e6d3d7bbd6cac4c58456b3d525e1
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-24 02:04:47 +0530

    feat: Add route parameter for single video page

--- src/App.js
+++ src/App.js
@@ -14,44 +14,46 @@ import {
 } from "./Pages";
 import { Routes, Route } from "react-router-dom";
 import { RequiresAuth } from "./Router/RequiresAuth";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signout" element={<Signout />} />
-        <Route path="/singlevideopage" element={<SingleVideoPage />} />
-        <Route
-          path="/liked"
-          element={<RequiresAuth children={<Liked />}></RequiresAuth>}
-        />
-        <Route
-          path="/watchlater"
-          element={<RequiresAuth children={<WatchLater />}></RequiresAuth>}
-        />
-        <Route
-          path="/history"
-          element={<RequiresAuth children={<History />}></RequiresAuth>}
-        />
-        <Route
-          path="/playlists"
-          element={<RequiresAuth children={<PlaylistPage />}></RequiresAuth>}
-        />
-        <Route
-          path="/playlists/:playlistID"
-          element={<RequiresAuth children={<SinglePlaylistPage />}></RequiresAuth>}
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-    <ToastContainer 
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signout" element={<Signout />} />
+          <Route path="/explore/:videoID" element={<SingleVideoPage />} />
+          <Route
+            path="/liked"
+            element={<RequiresAuth children={<Liked />}></RequiresAuth>}
+          />
+          <Route
+            path="/watchlater"
+            element={<RequiresAuth children={<WatchLater />}></RequiresAuth>}
+          />
+          <Route
+            path="/history"
+            element={<RequiresAuth children={<History />}></RequiresAuth>}
+          />
+          <Route
+            path="/playlists"
+            element={<RequiresAuth children={<PlaylistPage />}></RequiresAuth>}
+          />
+          <Route
+            path="/playlists/:playlistID"
+            element={
+              <RequiresAuth children={<SinglePlaylistPage />}></RequiresAuth>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <ToastContainer
         position="bottom-right"
         autoClose={3000}
         hideProgressBar={false}
@@ -61,10 +63,9 @@ function App() {
         rtl={false}
         pauseOnFocusLoss
         draggable
-        pauseOnHover 
+        pauseOnHover
         className="toaster-container"
-    
-    />
+      />
     </>
   );
 }
