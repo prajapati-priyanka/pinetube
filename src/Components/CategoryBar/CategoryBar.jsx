d4ba0434dad9fdcc94ffb5b9363586ee6cdae70e
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-24 16:22:26 +0530

    feat: Add categoryHandler

--- src/Components/CategoryBar/CategoryBar.jsx
+++ src/Components/CategoryBar/CategoryBar.jsx
@@ -1,10 +1,17 @@
 import "./CategoryBar.css"
 
-const CategoryBar = ({allCategories}) =>{
+const CategoryBar = ({allCategories, setAllCategories}) =>{
+    
+    const categoryHandler = (categoryName)=>{
+    setAllCategories(allCategories =>({
+        ...allCategories,
+         selectedCategory: categoryName
+    }))
+    }
     return(
    <div className="category-bar flex-container">
-       <button className="btn filter-category md-text">All</button>
-       {allCategories.map(category => <button key={category._id} className="btn filter-category md-text">{category.categoryName}</button> )}
+       <button className="btn filter-category md-text" onClick={()=>categoryHandler("")}>All</button>
+       {allCategories.categories.map(category => <button key={category._id} className="btn filter-category md-text" onClick={()=>categoryHandler(category.categoryName)}>{category.categoryName}</button> )}
     
     </div>
     )
