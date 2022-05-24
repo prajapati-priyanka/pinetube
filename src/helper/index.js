f6cdb041a7a7565ae58d0a94031daec6e966988c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-05-24 16:19:39 +0530

    feat: Add filter-helper

--- src/helper/index.js
+++ src/helper/index.js
@@ -2,3 +2,5 @@ export {checkIfAllInputIsPresent} from "./auth-helper";
 export {toggleLikeHandler} from "./like-helper";
 export { checkItemInArrayOfObject } from "./utility-helper";
 export {toggleWatchLaterHandler} from "./watchLater-helper";
+export {categoryData} from "./filter-helper";
+export {searchVideoData} from "./filter-helper"
