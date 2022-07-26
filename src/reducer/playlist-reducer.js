4c4f96c434bb9b963de66add53fcb4a921af3191
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-07-27 00:44:15 +0530

    feat: Update for add video and remove video case

--- src/reducer/playlist-reducer.js
+++ src/reducer/playlist-reducer.js
@@ -1,3 +1,11 @@
+const getNewPlaylist = (playlistId, playlists, playlistData) => {
+  return playlists.reduce(
+    (acc, curr) =>
+      curr._id === playlistId ? [...acc, playlistData] : [...acc, curr],
+    []
+  );
+};
+
 export const playlistReducer = (state, action) => {
   switch (action.type) {
     case "CREATE_PLAYLIST":
@@ -7,24 +15,31 @@ export const playlistReducer = (state, action) => {
       };
     case "ADD_TO_PLAYLIST":
       return {
-        playlists: state.playlists.map((item) =>
-          item.title === action.payload.title ? action.payload : item
+        ...state,
+        playlists: getNewPlaylist(
+          action.payload.playlistId,
+          state.playlists,
+          action.payload.playlistData
         ),
       };
     case "REMOVE_FROM_PLAYLIST":
       return {
-        playlists: action.payload
+        ...state,
+        playlists: getNewPlaylist(
+          action.payload.playlistId,
+          state.playlists,
+          action.payload.playlistData
+        ),
       };
     case "DELETE_PLAYLIST":
       return {
-        playlists: action.payload
-       
+        playlists: action.payload,
       };
     case "DELETE_FROM_SINGLE_PLAYLIST":
       return {
-      playlists: state.playlists.map((item) =>
-      item.title === action.payload.title ? action.payload : item) 
-       
+        playlists: state.playlists.map((item) =>
+          item.title === action.payload.title ? action.payload : item
+        ),
       };
 
     default:
